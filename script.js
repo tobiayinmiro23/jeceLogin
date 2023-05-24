1c3c24d95bcbabdb87cf51802bf460d1f02eb12a
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-05-24 15:29:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 let input=document.querySelectorAll('input')[0]
 let password=document.querySelectorAll('input')[1]
 let button=document.querySelectorAll('button')[0]
-
-const handleButton=()=>{
-    if(input.value === 'ayinmiro' && password.value === 'tobi'){
-       window.location='https://jece-project.vercel.app/'
-        return true
-
-    } else{
-        return false
-    }
-}
