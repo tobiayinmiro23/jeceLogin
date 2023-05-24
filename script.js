729633b51d4d30ab1d5e82b8820cd173e1901b44
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-05-24 14:14:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let password=document.querySelectorAll('input')[1]
 let button=document.querySelectorAll('button')[0]
 
 const handleButton=(e)=>{
-    if(input.value !== 'ayinmiro' && password.value !== 'tobi')  e.preventDefault()
+    if(input.value !== process.env.USERNAME && password.value !== process.env.PASSWORD)  e.preventDefault()
 
 }
 button.addEventListener('click',(e)=>{
