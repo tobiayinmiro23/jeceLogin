5d797d7500b0d100db08200cafdc8c874ac32913
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-05-25 09:17:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let input=document.querySelectorAll('input')[0]
 let password=document.querySelectorAll('input')[1]
 let button=document.querySelectorAll('button')[0]
+
+button.addEventListener('click',(e)=>{
+    if(input.value == process.env.USERNAME && password.value == process.env.PASSWORD){
+      console.log('welcome')
+  } else{
+      e.preventDefault()
+  }
+})
